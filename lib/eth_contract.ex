5cf342d2074c22841714e00b0c0b8c923df845c1
viktmv <viktmv@icloud.com>
2018-07-11 17:09:36 -0400

    update docs to add callback signatures

--- lib/eth_contract.ex
+++ lib/eth_contract.ex
@@ -13,14 +13,29 @@ defmodule EthContract do
   @callback balance_of(map()) :: integer()
   @callback owner_of(map()) :: any()
   @callback meta(map()) :: map()
+  @callback parse_abi(file_path :: String.t()) :: map()
+  @callback decode_data(
+    data :: String.t(),
+    abi :: String.t(),
+    method :: String.t(),
+    signatures_key :: String.t()
+  ) :: map()
+  @callback decode_log(
+    data :: String.t(),
+    topics :: String.t(),
+    abi :: String.t(),
+    method :: String.t()
+  ) :: map()
+  @callback total_supply(map()) :: integer()
 
   @doc """
-  Get the wallet address given a contract and a token id 
+  Get the wallet address given a contract and a token id
 
   ## Examples
 
-    iex> EthContract.owner_of(%{token_id: 1, contract: "0x06012c8cf97bead5deae237070f9587f8e7a266d"})
-    0x79bd592415ff6c91cfe69a7f9cd091354fc65a18 
+      iex> EthContract.owner_of(%{token_id: 1, contract: "0x06012c8cf97bead5deae237070f9587f8e7a266d"})
+      0x79bd592415ff6c91cfe69a7f9cd091354fc65a18
+
   """
   def owner_of(%{token_id: token_id, contract: contract}) do
     {:ok, address } = @json_rpc_client.eth_call(%{
@@ -71,6 +86,7 @@ defmodule EthContract do
     |> bytes_to_int
   end
 
+
   @doc """
   ERC721 Meta. This will return a Map with the meta information associated with a token. You have to provide the name of the method to use (ie. CryptoKitties uses getKitty)
 
@@ -80,7 +96,7 @@ defmodule EthContract do
       iex> EthContract.meta(%{token_id: 45, method: "getKitty", contract: "0x06012c8cf97BEaD5deAe237070F9587f8E7A266d", abi: abi})
       %{
         "birthTime" => 1511417999,
-        "cooldownIndex" => 0,  
+        "cooldownIndex" => 0,
         "generation" => 0,
         "genes" => 626837621154801616088980922659877168609154386318304496692374110716999053,
         "isGestating" => false,
@@ -90,7 +106,6 @@ defmodule EthContract do
         "sireId" => 0,
         "siringWithId" => 0
       }
-
   """
   def meta(%{token_id: token_id, method: method, contract: contract, abi: abi}) do
     {:ok, meta } = @json_rpc_client.eth_call(%{
@@ -102,14 +117,15 @@ defmodule EthContract do
     |> decode_data(abi, method, "outputs")
   end
 
+
   # Taken from https://github.com/hswick/exw3/blob/master/lib/exw3.ex#L159
   @doc """
   Parses abi into a map.
 
   ## Examples
-  
-    iex> abi = EthContract.parse_abi("test/support/crypto_kitties.json")
-    %{} 
+
+      iex> abi = EthContract.parse_abi("test/support/crypto_kitties.json")
+      %{}
   """
 
   def parse_abi(file_path) do
@@ -124,7 +140,7 @@ defmodule EthContract do
   defp trim_data(data) do
     {:ok, trimmed_output } = data
                              |> String.slice(2..-1)
-                             |> Base.decode16(case: :lower) 
+                             |> Base.decode16(case: :lower)
 
     trimmed_output
   end
@@ -138,6 +154,11 @@ defmodule EthContract do
 
     %{ output_signature: output_signature, output_names: output_names }
   end
+
+  @doc """
+  Decodes non-indexed data
+
+  """
 
   def decode_data(data, abi, method, signatures_key \\ "inputs") do
     trimmed_output  = trim_data(data)
@@ -164,9 +185,16 @@ defmodule EthContract do
     |> Enum.into(%{})
   end
 
+
+
+  @doc """
+  Decodes logs if indexed, if not - passes down to `decode_data`
+
+  """
+
   # This log is indexed
   # Take the signatures of the inputs that are not indexed => decode those as before
-  # Take the data that is indexed and decode that separately 
+  # Take the data that is indexed and decode that separately
   def decode_log(data, topics, abi, method) when Kernel.length(topics) > 1 do
     trimmed_data = trim_data(data)
 
@@ -191,7 +219,7 @@ defmodule EthContract do
 
     non_indexed_result = combine_data(decoded_names, decoded_data)
 
-    res = 
+    res =
       Enum.zip(indexed, topics)
       |> Enum.map(fn x ->
         { map, data } = x
@@ -209,7 +237,7 @@ defmodule EthContract do
         {map["name"], decoded}
       end)
 
-    indexed_result = res 
+    indexed_result = res
                      |> Enum.into(%{})
 
     Map.merge(indexed_result, non_indexed_result)
@@ -222,7 +250,7 @@ defmodule EthContract do
 
   defp decode_address(bytes) do
     {address, _} = TypeDecoder.decode_bytes(bytes, 40, :left)
-    "0x" <> address 
+    "0x" <> address
   end
 
   defp bytes_to_int("0x") do
