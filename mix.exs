7f3ef3352356454f5c3572b02b79b7f8cbffb3b7
Razvan <tzumby@gmail.com>
2018-07-11 23:09:52 -0400

    Bumped version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule EthContract.MixProject do
   def project do
     [
       app: :eth_contract,
-      version: "0.2.0",
+      version: "0.2.2",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       name: "ETHContract",
